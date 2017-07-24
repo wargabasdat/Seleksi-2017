2d5bc633679019fc03d912426e91d2534e92368e
priskar <priska_reysa@yahoo.com>
2017-07-24 20:24:14 +0700

    grafik garis pengguna internet di negara asean

---
+++ javascript/pengguna-malaysia.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+	var chart = $("#line-chartcanvas");
+
+	var jumlah = {
+		labels : ["2014", "2015", "2016"],
+		datasets : [
+			{
+				label : "",
+				data : [pengguna_malaysia_2014, pengguna_malaysia_2015, pengguna_malaysia_2016],
+				backgroundColor : "blue",
+				borderColor : ["rgb(177, 201, 249)"],
+				fill : false,
+				lineTension : 0,
+				pointRadius : 5
+			}
+		]
+	};
+
+	var options = {
+		title : {
+			display : true,
+			position : "top",
+			text : "Pengguna Internet di Malaysia",
+			fontSize : 18,
+			fontColor : "#111"
+		},
+		legend : {
+			display : false
+		},
+		scales: {
+			yAxes: [{
+				ticks: {
+					beginAtZero: true
+				}
+			}]
+		},
+	};
+
+	var chart = new Chart( chart, {
+		type : "line",
+		data : jumlah,
+		options : options
+	} );
+	
+});
