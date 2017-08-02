b892463fe05de802dc9c71bf97f316dff63621c5
Vincent Endrahadi <13515117@std.stei.itb.ac.id>
2017-08-02 22:58:21 +0700

    Add mainpage

---
+++ DV4/resources/views/home.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../public/favicon.ico">
+
+    <title>Tempat Wisata Bali</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="/public/app.css" rel="stylesheet">
+  </head>
+
+<body>
+
+    @include('layouts.nav')
+
+
+    <br>
+    <br>
+
+    <div class="container" background="red">
+        <div align="center">
+            <img src="http://www.fabulousubud.com/admin/images/a3795e908a28014eccf26bc8b7412566.jpg" alt="bali" id="bali" class="img-responsive" width="auto" height="500">
+        </div>
+    </div>
+
+
+
+
+</body>
+</html>
---
+++ DV4/resources/views/layouts/nav.blade.php
@@ -0,0 +1,11 @@
+    <nav class="navbar fix-top navbar-inverse bg-inverse navbar-toggleable-md">
+        <h2 class="navbar-brand" href="#">Dashboard</h2>
+        <div class="collapse navbar-collapse">
+            <div class="navbar-nav">
+                <a href="#" class="nav-item nav-link active">Home</a>
+                <a href="#" class="nav-item nav-link">List</a>
+                <a href="#" class="nav-item nav-link">About</a>
+
+            </div>
+        </div>        
+    </nav>
